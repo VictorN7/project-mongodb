51a5a50176b1dc2cb5e6ecf1f2ff3deb1d8a3af5
VictorN7 <victorhnogueira@hotmail.com>
2026-02-17 16:01:53 -0300

    Create class DTO for User

--- src/main/java/com/victornogueira/projectmongo/resources/UserResource.java
+++ src/main/java/com/victornogueira/projectmongo/resources/UserResource.java
@@ -1,6 +1,8 @@
 package com.victornogueira.projectmongo.resources;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.victornogueira.projectmongo.domain.User;
+import com.victornogueira.projectmongo.dto.UserDto;
 import com.victornogueira.projectmongo.services.UserService;
 
 @RestController
@@ -22,13 +24,13 @@ public class UserResource {
 	}
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
-		return ResponseEntity.ok().body(userService.findAll());
+	public ResponseEntity<List<UserDto>> findAll(){
+		return ResponseEntity.ok().body(userService.findAll().stream().map(x -> new UserDto(x)).collect(Collectors.toList()));
 	}
 
 	@GetMapping("/{id}")
-	public ResponseEntity<User> findById(@PathVariable String id) {
-		return ResponseEntity.ok().body(userService.findById(id));
+	public ResponseEntity<UserDto> findById(@PathVariable String id) {
+		return ResponseEntity.ok().body(new UserDto(userService.findById(id)));
 	}
 	
 }
