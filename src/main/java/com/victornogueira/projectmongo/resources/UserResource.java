13080b689aeaf53c1b3f5e6156757f9074353356
VictorN7 <victorhnogueira@hotmail.com>
2026-02-18 12:04:44 -0300

    Create endpoint DELETE for users

--- src/main/java/com/victornogueira/projectmongo/resources/UserResource.java
+++ src/main/java/com/victornogueira/projectmongo/resources/UserResource.java
@@ -1,10 +1,10 @@
 package com.victornogueira.projectmongo.resources;
 
-import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,6 +45,13 @@ public class UserResource {
 		
 		return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri()).body(new UserDto(user));
 	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id){
+		userService.deleteUser(id);
+		return ResponseEntity.noContent().build();
+	}
+	
 	
 	
 }
--- src/main/java/com/victornogueira/projectmongo/services/UserService.java
+++ src/main/java/com/victornogueira/projectmongo/services/UserService.java
@@ -13,30 +13,31 @@ import com.victornogueira.projectmongo.services.exception.ObjectNotFoundExceptio
 public class UserService {
 
 	private UserRepository userRepository;
-	
+
 	public UserService(UserRepository userRepository) {
 		this.userRepository = userRepository;
 	}
-	
-	public List<User> findAll(){
-		
+
+	public List<User> findAll() {
 		List<User> listUsers = userRepository.findAll();
 		return listUsers;
 	}
 
 	public User findById(String id) {
-	
 		return userRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException(id));
 	}
-	
+
 	public User insertUser(User user) {
-		
 		return userRepository.insert(user);
-		
 	}
-	
+
+	public void deleteUser(String id) {
+		User user = findById(id);
+		userRepository.delete(user);
+	}
+
 	public User fromDto(UserDto userDto) {
 		return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
 	}
-	
+
 }
